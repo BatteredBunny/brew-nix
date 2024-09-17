443196dbc7e9d06526ee969197624b738baf48b5
Luke Channings <461449+LukeChannings@users.noreply.github.com>
2024-09-17 15:09:35 +0100

    Don't patch shebangs

--- casks.nix
+++ casks.nix
@@ -52,6 +52,9 @@ let
 
     sourceRoot = lib.optionalString (hasApp cask) (getApp cask);
 
+    # Patching shebangs invalidates code signing
+    dontPatchShebangs = true;
+
     installPhase =
       if (hasPkg cask) then ''
         mkdir -p $out/Applications
