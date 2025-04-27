928e5382d927727666bcbbd27240e73eeb269b4a
isabel <isabel@isabelroses.com>
2025-04-27 22:30:24 +0100

    the refactor ever :D (#20)
    * chore: fmt with nixfmt-rfc-style
    
    * refactor: remove `with lib`
    
    * refactor: remove fu
    
    * refactor: casks.nix inherits stdenv and lib from pkgs
    
    * chore: update nix-darwin url
    
    * refactor: use finalAttrs & simplify where possible
    
    * perf: memorize has*
    
    * perf: memorize artifacts
    
    * chore: update example

--- casks.nix
+++ casks.nix
@@ -1,97 +1,125 @@
-{ pkgs, lib, brew-api, stdenv, ... }:
+{
+  pkgs,
+  lib ? pkgs.lib,
+  brew-api,
+  stdenv ? pkgs.stdenv,
+  ...
+}:
 let
-  hasBinary = cask: lib.hasAttr "binary" (getArtifacts cask);
-  hasApp = cask: lib.hasAttr "app" (getArtifacts cask);
-  hasPkg = cask: lib.hasAttr "pkg" (getArtifacts cask);
-
   getName = cask: builtins.elemAt cask.name 0;
+  getBinary = artifacts: builtins.elemAt artifacts.binary 0;
+  getApp = artifacts: builtins.elemAt artifacts.app 0;
 
-  getBinary = cask: builtins.elemAt (getArtifacts cask).binary 0;
-  getApp = cask: builtins.elemAt (getArtifacts cask).app 0;
-  getArtifacts = cask: lib.mergeAttrsList cask.artifacts;
+  caskToDerivation =
+    cask:
+    let
+      artifacts = lib.mergeAttrsList cask.artifacts;
+      isBinary = lib.hasAttr "binary" artifacts;
+      isApp = lib.hasAttr "app" artifacts;
+      isPkg = lib.hasAttr "pkg" artifacts;
+    in
+    stdenv.mkDerivation (finalAttrs: {
+      pname = cask.token;
+      inherit (cask) version;
 
-  caskToDerivation = cask: stdenv.mkDerivation rec {
-    pname = cask.token;
-    version = cask.version;
+      src = pkgs.fetchurl {
+        inherit (cask) url;
+        sha256 = lib.optionalString (cask.sha256 != "no_check") cask.sha256;
+      };
 
-    src = pkgs.fetchurl {
-      url = cask.url;
-      sha256 = lib.optionalString (cask.sha256 != "no_check") cask.sha256;
-    };
+      nativeBuildInputs =
+        with pkgs;
+        [
+          undmg
+          unzip
+          gzip
+          _7zz
+          makeWrapper
+        ]
+        ++ lib.optional isPkg (
+          with pkgs;
+          [
+            xar
+            cpio
+          ]
+        );
 
-    nativeBuildInputs = with pkgs; [
-      undmg
-      unzip
-      gzip
-      _7zz
-      makeWrapper
-    ] ++ lib.optional (hasPkg cask) (with pkgs; [
-      xar
-      cpio
-    ]);
+      unpackPhase =
+        if isPkg then
+          ''
+            xar -xf $src
+            for pkg in $(cat Distribution | grep -oE "#.+\.pkg" | sed -e "s/^#//" -e "s/$/\/Payload/"); do
+              zcat $pkg | cpio -i
+            done
+          ''
+        else if isApp then
+          ''
+            undmg $src || 7zz x -snld $src
+          ''
+        else if isBinary then
+          ''
+            if [ "$(file --mime-type -b "$src")" == "application/gzip" ]; then
+              gunzip $src -c > ${getBinary artifacts}
+            elif [ "$(file --mime-type -b "$src")" == "application/x-mach-binary" ]; then
+              cp $src ${getBinary artifacts}
+            fi
+          ''
+        else
+          "";
 
-    unpackPhase =
-      if (hasPkg cask) then ''
-        xar -xf $src
-        for pkg in $(cat Distribution | grep -oE "#.+\.pkg" | sed -e "s/^#//" -e "s/$/\/Payload/"); do
-          zcat $pkg | cpio -i
-        done
-      '' else if (hasApp cask) then ''
-        undmg $src || 7zz x -snld $src
-      '' else if (hasBinary cask) then ''
-        if [ "$(file --mime-type -b "$src")" == "application/gzip" ]; then
-          gunzip $src -c > ${getBinary cask}
-        elif [ "$(file --mime-type -b "$src")" == "application/x-mach-binary" ]; then
-          cp $src ${getBinary cask}
-        fi
-      '' else "";
+      sourceRoot = lib.optionalString isApp (getApp artifacts);
 
-    sourceRoot = lib.optionalString (hasApp cask) (getApp cask);
+      # Patching shebangs invalidates code signing
+      dontPatchShebangs = true;
 
-    # Patching shebangs invalidates code signing
-    dontPatchShebangs = true;
+      installPhase =
+        if isPkg then
+          ''
+            mkdir -p $out/Applications
+            cp -R Applications/* $out/Applications/
 
-    installPhase =
-      if (hasPkg cask) then ''
-        mkdir -p $out/Applications
-        cp -R Applications/* $out/Applications/
+            if [ -d "Resources" ]; then
+              mkdir -p $out/Resources
+              cp -R Resources/* $out/Resources/
+            fi
 
-        if [ -d "Resources" ]; then
-          mkdir -p $out/Resources
-          cp -R Resources/* $out/Resources/
-        fi
+            if [ -d "Library" ]; then
+              mkdir -p $out/Library
+              cp -R Library/* $out/Library/
+            fi
+          ''
+        else if isApp then
+          ''
+            mkdir -p "$out/Applications/${finalAttrs.sourceRoot}"
+            cp -R . "$out/Applications/${finalAttrs.sourceRoot}"
 
-        if [ -d "Library" ]; then
-          mkdir -p $out/Library
-          cp -R Library/* $out/Library/
-        fi
-      '' else if (hasApp cask) then ''
-        mkdir -p "$out/Applications/${sourceRoot}"
-        cp -R . "$out/Applications/${sourceRoot}"
+            if [[ -e "$out/Applications/${finalAttrs.sourceRoot}/Contents/MacOS/${getName cask}" ]]; then
+              makeWrapper "$out/Applications/${finalAttrs.sourceRoot}/Contents/MacOS/${getName cask}" $out/bin/${cask.token}
+            elif [[ -e "$out/Applications/${finalAttrs.sourceRoot}/Contents/MacOS/${lib.removeSuffix ".app" finalAttrs.sourceRoot}" ]]; then
+              makeWrapper "$out/Applications/${finalAttrs.sourceRoot}/Contents/MacOS/${lib.removeSuffix ".app" finalAttrs.sourceRoot}" $out/bin/${cask.token}
+            fi
+          ''
+        else if (isBinary && !isApp) then
+          ''
+            mkdir -p $out/bin
+            cp -R ./* $out/bin
+          ''
+        else
+          "";
 
-        if [[ -e "$out/Applications/${sourceRoot}/Contents/MacOS/${getName cask}" ]]; then
-          makeWrapper "$out/Applications/${sourceRoot}/Contents/MacOS/${getName cask}" $out/bin/${cask.token}
-        elif [[ -e "$out/Applications/${sourceRoot}/Contents/MacOS/${lib.removeSuffix ".app" sourceRoot}" ]]; then
-          makeWrapper "$out/Applications/${sourceRoot}/Contents/MacOS/${lib.removeSuffix ".app" sourceRoot}" $out/bin/${cask.token}
-        fi
-      '' else if (hasBinary cask && !hasApp cask) then ''
-        mkdir -p $out/bin
-        cp -R ./* $out/bin
-      '' else "";
+      meta = {
+        inherit (cask) homepage;
+        description = cask.desc;
+        platforms = lib.platforms.darwin;
+        mainProgram = if (isBinary && !isApp) then (getBinary artifacts) else cask.token;
+      };
+    });
 
-    meta = {
-      homepage = cask.homepage;
-      description = cask.desc;
-      platforms = lib.platforms.darwin;
-      mainProgram = if (hasBinary cask && !hasApp cask) then (getBinary cask) else (cask.token);
-    };
-  };
-
-  casks = builtins.fromJSON (builtins.readFile (brew-api + "/cask.json"));
+  casks = lib.importJSON (brew-api + "/cask.json");
 in
-builtins.listToAttrs (builtins.map
-  (cask: {
+lib.listToAttrs (
+  builtins.map (cask: {
     name = cask.token;
     value = caskToDerivation cask;
-  })
-  casks)
+  }) casks
+)
--- examples/flake.nix
+++ examples/flake.nix
@@ -3,18 +3,16 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
-    flake-utils.url = "github:numtide/flake-utils";
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     brew-nix = {
-      # for local testing via `nix flake check` while developing 
+      # for local testing via `nix flake check` while developing
       #url = "path:../";
       url = "github:BatteredBunny/brew-nix";
       inputs.nix-darwin.follows = "nix-darwin";
       inputs.brew-api.follows = "brew-api";
-      inputs.flake-utils.follows = "flake-utils";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     brew-api = {
@@ -23,18 +21,27 @@
     };
   };
 
-  outputs = { self, nixpkgs, nix-darwin, brew-nix, ... }: {
-    darwinConfigurations.somehost = nix-darwin.lib.darwinSystem {
-      system = "x86_64-darwin";
-      modules = [
-        brew-nix.darwinModules.default
-        ({ pkgs, ... }: {
-          brew-nix.enable = true;
-          environment.systemPackages = [
-            pkgs.brewCasks.marta
-          ];
-        })
-      ];
+  outputs =
+    {
+      nix-darwin,
+      brew-nix,
+      ...
+    }:
+    {
+      darwinConfigurations.somehost = nix-darwin.lib.darwinSystem {
+        system = "x86_64-darwin";
+        modules = [
+          brew-nix.darwinModules.default
+          (
+            { pkgs, ... }:
+            {
+              brew-nix.enable = true;
+              environment.systemPackages = [
+                pkgs.brewCasks.marta
+              ];
+            }
+          )
+        ];
+      };
     };
-  };
 }
