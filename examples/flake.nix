a7c6bc9c4759d07d171c38004171c048f943f7d7
isabel <isabel@isabelroses.com>
2025-04-27 22:06:47 +0100

    chore: update example

--- examples/flake.nix
+++ examples/flake.nix
@@ -3,18 +3,16 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
-    flake-utils.url = "github:numtide/flake-utils";
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     brew-nix = {
-      # for local testing via `nix flake check` while developing 
+      # for local testing via `nix flake check` while developing
       #url = "path:../";
       url = "github:BatteredBunny/brew-nix";
       inputs.nix-darwin.follows = "nix-darwin";
       inputs.brew-api.follows = "brew-api";
-      inputs.flake-utils.follows = "flake-utils";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     brew-api = {
@@ -25,8 +23,6 @@
 
   outputs =
     {
-      self,
-      nixpkgs,
       nix-darwin,
       brew-nix,
       ...
